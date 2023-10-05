929afc3a6dbdeb7abaa8b1b96b1a7e5e25eb90ac
linsen <15840090831@163.com>
2023-10-05 13:37:09 +0800

    v1

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="app">
-    <p>xiugaixiugai</p>
+    <p>v1</p>
     <!-- 显示数据组件 -->
     <counterView :num="num"></counterView>
     <!-- 显示按钮组件 -->
@@ -25,7 +25,7 @@ export default {
       num.value = newNum
     }
     return {
-      num,getNewNum
+      num,getNewNum,pace
     }
   }
 }
